63a67a08bb8331c5d7c96a94d53e5b8cb3160f90
Alexander Recalde <apache@vps158787.vps.ovh.ca>
2017-11-30 02:34:15 -0500

    Styling Commit and Search Enhance

--- index.php
+++ index.php
@@ -2,14 +2,34 @@
       <link type="text/css" rel="stylesheet" href="http://gigahornet.com/css/bootstrap.min.css">
       <style>
         input{
-          width: 100%;
-        }
-        #qa div {
-          margin: 10px;
-          width: 90%;
-          
+          width: 60%;
+          height: 50px;
+          font-size: 40px;
+          margin: 3px;
         }
 
+::-webkit-input-placeholder {
+   text-align: center;
+    font-size: 40px;
+}
+
+:-moz-placeholder { /* Firefox 18- */
+   text-align: center;  
+      font-size: 40px;
+
+}
+
+::-moz-placeholder {  /* Firefox 19+ */
+   text-align: center;  
+    font-size: 40px;
+
+        }
+
+:-ms-input-placeholder {  
+   text-align: center; 
+    font-size: 40px;
+
+        }
   </style>
 </head>
 
@@ -18,57 +38,36 @@
   include 'includes/connect.php';
 
 
-$NUMOFIANSWERS = 6; //minus 1 
+$NUMOFIANSWERS = 0; //minus 1 
 ?>
+<center> <h1>
+      Question Bank Search
+    </h1>
 
+    <br />
+<form method='post'>
+  <input type='text' name='question' placeholder='Question' /><br /><br />
+  <input type='text' name='answer' placeholder='Answer' /><br />
+  <h3>
+    
+  </h3><br />
+<?php 
+  $count = 1;
+  while($count < $NUMOFIANSWERS) {
+    echo "<input type='text' name='ianswer$count' placeholder='Incorrect Answer $count' /><br />";
+    $count++;
+  }
+?>
 <div class='col-md-12' style='border-style: solid; border-width: 1px; border-color: black'>
   <div class='col-md-6'>
-    <h1>
-      Add
-    </h1>
-    <br />
-    <form method='post'>
-      <input type='text' name='question' placeholder='Question' /><br />
-      <input type='text' name='answer' placeholder='Answer' /><br />
-      <h3>
-        Optional
-      </h3><br />
-        <?php 
-      $count = 1;
-      while($count < 6) //show 5
-      {
-                echo "<input type='text' name='ianswer$count' placeholder='Incorrect Answer $count' /><br />";
-        $count++;
-      }
-      ?>
       <input type='submit' value='Add' name='add' />
-
-    </form>
   </div>
   <div class='col-md-6'>
-    <h1>
-      Search
-    </h1>
-    <br />
-     <form method='post'>
-             <h3>
-        Only one field required for search
-      </h3><br />
-      <input type='text' name='question' placeholder='Question' /><br />
-      <input type='text' name='answer' placeholder='Answer' /><br />
-
-        <?php 
-      $count = 1;
-      while($count < $NUMOFIANSWERS) //show 5
-      {
-                echo "<input type='text' name='ianswer$count' placeholder='Incorrect Answer $count' /><br />";
-        $count++;
-      }
-      ?>
       <input type='submit' value='Search' name='search' />
-    </form>
   </div>
-</div><br /><br />
+</div>
+</form><br /><br />
+</center>
 
 <?php 
   $addBtn = $_POST['add'];
@@ -101,7 +100,9 @@ $size = $count-2;
     if(isset($search)){
     //$str = arrayToStr($ianswer, $size);
     //$sql = "INSERT INTO `qa`.`questions` (`id`, `question`, `answer`, `ianswer`) VALUES (NULL, '$question', '$answer', '$str');";
-    $sql = "SELECT * FROM `questions` WHERE `question`='$question' OR `answer`='$answer'";
+    //$sql = "SELECT * FROM `questions` WHERE `question`='$question' OR `answer`='$answer'";
+          $sql = "SELECT * FROM `questions` WHERE (`question` LIKE '%%".$question."%%' AND `answer` LIKE '%%".$answer."%%')";
+
       $query = mysqli_query($connect, $sql);
     if($query){
       while($results = mysqli_fetch_assoc($query)){
@@ -120,6 +121,7 @@ $size = $count-2;
   if(isset($addBtn)){
     $str = arrayToStr($ianswer, $size);
     $sql = "INSERT INTO `qa`.`questions` (`id`, `question`, `answer`, `ianswer`) VALUES (NULL, '$question', '$answer', '$str');";
+
     $query = mysqli_query($connect, $sql);
     if($query){
       echo "Successfully added to the database";
